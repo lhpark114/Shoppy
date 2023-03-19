8863b3dc1968a2fbd262cdd89eaeba1b444c32bf
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-19 17:55:49 -0400

    NewProduct - CSS

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -38,13 +38,11 @@ export default function NewProduct() {
   };
 
   return (
-    <section>
-      <header>
-        <h2>새로운 제품 등록</h2>
-        {success && <p>✅{success}</p>}
-      </header>
-      {file && <img src={URL.createObjectURL(file)} alt='local file' />}
-      <form onSubmit={handleSubmit}>
+    <section className='w-full text-center'>
+        <h2 className='text-2xl font-bold my-4'>새로운 제품 등록</h2>
+        {success && <p className='my-2'>✅{success}</p>}
+      {file && <img className='w-96 mx-auto mb-2' src={URL.createObjectURL(file)} alt='local file' />}
+      <form className='flex flex-col px-12' onSubmit={handleSubmit}>
         <input
           type='file'
           placeholder='No File Chosen'
