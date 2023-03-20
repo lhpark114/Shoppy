16fed59e2229e81af5a3e87c2ceba05a6477cba1
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-19 23:06:56 -0400

    Product detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 export default function ProductDetail() {
-    const { productId } = useParams();
-    return (
-        <div>
-            ProductsDetail {productId}
-        </div>
-    );
-}
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleSelect = (e) => setSelected(e.target.value);
+  const handleClick = (e) => {
+    // 여기서 장바구니에 추가하면 됨!
+  };
+  return (
+    <>
+      <p className='mx-12 mt-4 text-gray-700'>{category}</p>
+      <section className='flex flex-col md:flex-row p-4'>
+        <img className='w-full px-4 basis-7/12' src={image} alt={title}></img>
+        <div className='w-full basis-5/12 flex flex-col p-4'>
+          <h2 className='text-3xl font-bold py-2 border-b border-gray-400'>
+            {title}
+          </h2>
+          <p lassName='text-2xl font-bold py-2 border-b border-gray-400'>
+            ₩{price}
+          </p>
+          <p lassName='py-4 text-lg'>{description}</p>
 
+          <div className='flex items-center'>
+            <label className='text-brand font-bold' htmlFor='select'>
+              옵션:{' '}
+            </label>
+            <select
+              id='select'
+              className='p-2 m-2 flex-1 border-2 border-dashed border-brand outline-none'
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((option, index) => (
+                  <option key={index}>{option}</option>
+                ))}
+            </select>
+          </div>
+
+          <Button text='장바구니에 추가' onClick={handleClick} />
+        </div>
+      </section>
+    </>
+  );
+}
