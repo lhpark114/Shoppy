21f917d8cd44c448437c755c5b0d1458e340abb6
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-21 15:15:41 -0400

    useMutate

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+
+  const queryClient = useQueryClient();
+  const addProduct = useMutation(
+    ({product, url}) => addNewProduct(product, url),
+    {
+    onSuccess: () => queryClient.invalidateQueries()(['products']),
+  });
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -26,16 +34,19 @@ export default function NewProduct() {
     setIsUploading(true);
     uploadImage(file)//
     .then((url) => {
-      addNewProduct(product, url)
-      .then(()=>{
-        setSuccess('성공적으로 제품이 추가되었습니다.');
-        setTimeout(()=> {
-            setSuccess(null);
+      addProduct.mutate(
+        {product, url}, {
+          onSuccess: () => {
+            setSuccess('성공적으로 제품이 추가되었습니다.');
+            setTimeout(()=> {
+              setSuccess(null);
         }, 4000);
-      });
-    })
-    .finally(() => setIsUploading(false));
-  };
+      },}
+      );
+      })
+      .finally(() => setIsUploading(false));
+    }
+    
 
   return (
     <section className='w-full text-center'>
