eb01f57bea364b4e2aea15dc53dc25b3cdc882b1
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-20 21:57:31 -0400

    Cart - CSS

--- src/components/ui/CartItem.jsx
+++ src/components/ui/CartItem.jsx
@@ -3,6 +3,8 @@ import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
 import { RiDeleteBin5Fill } from 'react-icons/ri';
 import { addOrUpdateToCart, removeFromCart } from '../../api/firebase';
 
+const ICON_CLASS = 'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1'
+
 export default function CartItem({
   product,
   product: { id, image, title, option, quantity, price },
@@ -12,23 +14,26 @@ export default function CartItem({
     if (quantity < 2) return;
     addOrUpdateToCart(uid, { ...product, quantity: quantity - 1 });
   };
-  
+
   const handlePlus = () =>
     addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 });
 
   const handleDelete = () => removeFromCart(uid, id);
 
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{option}</p>
-        <div>
-          <AiOutlineMinusSquare onClick={handleMinus} />
+    <li className='flex justify-between my-2 items-center'>
+      <img className='w-24 md:w-48 rounded-lg' src={image} alt={title} />
+      <div className='flex-1 flex justify-between ml-4'>
+        <div className='basis-3/5'>
+            <p className='text-lg'>{title}</p>
+            <p className='text-xl font-bold text-brand'>{option}</p>
+            <p>{price}</p>
+        </div>
+        <div className='text-2xl flex items-center'>
+          <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
           <span>{quantity}</span>
-          <AiOutlinePlusSquare onClick={handlePlus} />
-          <RiDeleteBin5Fill onClick={handleDelete} />
+          <AiOutlinePlusSquare className={ICON_CLASS} onClick={handlePlus} />
+          <RiDeleteBin5Fill className={ICON_CLASS} onClick={handleDelete} />
         </div>
       </div>
     </li>
